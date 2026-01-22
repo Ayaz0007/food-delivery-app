164d898cd40827e74d7fbe1f46814ffde0947660
Ayaz0007 <142489007+Ayaz0007@users.noreply.github.com>
2026-01-22 17:44:40 +0530

    chore: Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/food-delivery-app/',
   plugins: [react()],
   server: {
     historyApiFallback: true,
